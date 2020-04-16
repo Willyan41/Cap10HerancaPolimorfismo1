9f11d250939b1b27c4ea820a1256c7b6ae9cbee6
Willyan Santos <willyan41@hotmail.com>
2020-04-15 23:51:40 -0300

    Adicionar arquivos de projeto.

---
+++ Cap10HerancaPolimorfismo1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using Cap10HerancaPolimorfismo1.Entities;
+using System.Collections.Generic;
+
+namespace Cap10HerancaPolimorfismo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsourced (y/n): ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'y')
+                {
+                    Console.Write("Additional Charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new OutSourceEmployee(name, hours, valuePerHour, additionalCharge));
+
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+
+            }
+            Console.WriteLine("PAYMENTS");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment());
+
+            }
+        }
+    }
+}
